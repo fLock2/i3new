6ccc16ea9badba257bd210c5091bc55ad2c946de
Trevor <35179074+fLock2@users.noreply.github.com>
2021-04-29 22:50:25 -0400

    Add files via upload

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,19 +1,11 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-# Bar is the name set in the polybar config, so if you change it, you have to change it here too.
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
-  done
-else
-  polybar --reload top &
-fi
+polybar -rq bar &
 
-echo "Bars launched..."
+echo "Polybar launched..."
